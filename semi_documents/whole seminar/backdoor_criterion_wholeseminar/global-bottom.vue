588e4c2e80a4b8b984b74e46171158b25b615476
morinota <morinota416@gmail.com>
2022-11-13 16:38:39 +0900

    delete some dir and files

--- semi_documents/whole seminar/backdoor_criterion_wholeseminar/global-bottom.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <Progress
-    v-if="$slidev.nav.currentPage !== 1 && $slidev.nav.currentLayout!== 'end'"
-    :current="$slidev.nav.currentPage"
-    :total="$slidev.nav.total" />
-</template>
