8ee7fa4864c22e5ae385a031a308c9a0b034e39b
morinota <morinota416@gmail.com>
2023-05-13 09:24:41 +0900

    add papers

--- machine_learning/recomend_system/metrics/paper_RADio_rank_aware_divergence_metrics_to_measure_normative_diversity_in_news_recommendations/divergence_metrics.py
+++ machine_learning/recomend_system/metrics/paper_RADio_rank_aware_divergence_metrics_to_measure_normative_diversity_in_news_recommendations/divergence_metrics.py
@@ -66,21 +66,16 @@ class JSDivergence(DivergenceMetricAbstract):
 
 @dataclasses.dataclass
 class RankAwareProbabilityMassFunction:
-    dist: Dict[Any, float]
+    pmf: Dict[Any, float]
 
     @classmethod
-    def from_ranking(cls, R: List[Any], method: str = "MMR") -> Dict[Any, float]:
+    def from_ranking(cls, R: List[Any], method: str = "MMR") -> "RankAwareProbabilityMassFunction":
         rank_aware_pmf = defaultdict(float)
-        rank_weights_sum = sum(
-            [cls._calc_rank_weight(rank_idx + 1, method) for rank_idx in range(len(R))]
-        )
+        rank_weights_sum = sum([cls._calc_rank_weight(rank_idx + 1, method) for rank_idx in range(len(R))])
         for rank_idx in range(len(R)):
             rank = rank_idx + 1
-            rank_aware_pmf[R[rank_idx]] += (
-                cls._calc_rank_weight(rank, method) / rank_weights_sum
-            )
-            print(rank_aware_pmf[R[rank_idx]])
-        return rank_aware_pmf
+            rank_aware_pmf[R[rank_idx]] += cls._calc_rank_weight(rank, method) / rank_weights_sum
+        return RankAwareProbabilityMassFunction(rank_aware_pmf)
 
     @classmethod
     def _calc_rank_weight(cls, rank: int, method: str = "MMR") -> float:
--- machine_learning/recomend_system/metrics/paper_RADio_rank_aware_divergence_metrics_to_measure_normative_diversity_in_news_recommendations/test_divergence_metrics.py
+++ machine_learning/recomend_system/metrics/paper_RADio_rank_aware_divergence_metrics_to_measure_normative_diversity_in_news_recommendations/test_divergence_metrics.py
@@ -1,25 +1,18 @@
 import math
 
-from divergence_metrics import (
-    JSDivergence,
-    KLDivergence,
-    RankAwareProbabilityMassFunction,
-)
-from sample import (
-    calc_f_JS_t,
-    calc_rank_aware_JS_divergence,
-    calc_rank_aware_pmf,
-    calc_rank_weight_MMR,
-    calc_rank_weight_nDCG,
-)
+from divergence_metrics import JSDivergence, KLDivergence, RankAwareProbabilityMassFunction
+
+
+def calc_rank_weight_MMR(rank: int) -> float:
+    return 1 / rank
 
 
 def test_calc_KL_divergence() -> None:
     P = {"a": 0.2, "b": 0.3, "c": 0.5}
     Q = {"a": 0.3, "b": 0.3, "c": 0.4}
-    kl_div_PQ_expected = -(
-        0.2 * math.log2(0.3) + 0.3 * math.log2(0.3) + 0.5 * math.log2(0.4)
-    ) + (0.2 * math.log2(0.2) + 0.3 * math.log2(0.3) + 0.5 * math.log2(0.5))
+    kl_div_PQ_expected = -(0.2 * math.log2(0.3) + 0.3 * math.log2(0.3) + 0.5 * math.log2(0.4)) + (
+        0.2 * math.log2(0.2) + 0.3 * math.log2(0.3) + 0.5 * math.log2(0.5)
+    )
 
     calculator = KLDivergence()
     kl_div_PQ_actual = calculator.calc(P, Q)
@@ -27,7 +20,7 @@ def test_calc_KL_divergence() -> None:
         kl_div_PQ_actual,
         kl_div_PQ_expected,
         rel_tol=1e-3,
-    )  # validな確率分布にする調整(_convert_to_valid_dist)によりやや値は異なる.
+    )  # The value varies slightly depending on the adjustment (JSDivergence._convert_to_valid_dist) to make the probability distribution valid.
 
 
 def test_calc_JS_divergence() -> None:
@@ -39,12 +32,8 @@ def test_calc_JS_divergence() -> None:
         - (0.3 + 0.3) / 2 * math.log2((0.3 + 0.3) / 2)
         - (0.5 + 0.4) / 2 * math.log2((0.5 + 0.4) / 2)
     )
-    js_div_second_term = (
-        1 / 2 * (0.2 * math.log2(0.2) + 0.3 * math.log2(0.3) + 0.5 * math.log2(0.5))
-    )
-    js_div_third_term = (
-        1 / 2 * (0.3 * math.log2(0.3) + 0.3 * math.log2(0.3) + 0.4 * math.log2(0.4))
-    )
+    js_div_second_term = 1 / 2 * (0.2 * math.log2(0.2) + 0.3 * math.log2(0.3) + 0.5 * math.log2(0.5))
+    js_div_third_term = 1 / 2 * (0.3 * math.log2(0.3) + 0.3 * math.log2(0.3) + 0.4 * math.log2(0.4))
     js_div_PQ_expected = js_div_first_term + js_div_second_term + js_div_third_term
 
     calculator = JSDivergence()
@@ -53,14 +42,13 @@ def test_calc_JS_divergence() -> None:
         js_div_PQ_actual,
         js_div_PQ_expected,
         rel_tol=1e-3,
-    )  # validな確率分布にする調整(_convert_to_valid_dist)によりやや値は異なる.
+    )  # The value varies slightly depending on the adjustment (JSDivergence._convert_to_valid_dist) to make the probability distribution valid.
 
 
 def test_calc_rank_aware_pmf() -> None:
     R = ["a", "b", "b"]
     Q_asterisk_expected = {
-        "a": calc_rank_weight_MMR(1)
-        / (calc_rank_weight_MMR(1) + calc_rank_weight_MMR(2) + calc_rank_weight_MMR(3)),
+        "a": calc_rank_weight_MMR(1) / (calc_rank_weight_MMR(1) + calc_rank_weight_MMR(2) + calc_rank_weight_MMR(3)),
         "b": (calc_rank_weight_MMR(2) + calc_rank_weight_MMR(3))
         / (calc_rank_weight_MMR(1) + calc_rank_weight_MMR(2) + calc_rank_weight_MMR(3)),
     }
@@ -68,25 +56,3 @@ def test_calc_rank_aware_pmf() -> None:
     Q_asterisk_actual = RankAwareProbabilityMassFunction.from_ranking(R)
     assert Q_asterisk_actual == Q_asterisk_expected
     assert sum(Q_asterisk_actual.values()) == 1.0
-
-
-def test_calc_f_JS_t() -> None:
-    t = 1
-    f_JS_t_expected = 1 / 2 * ((t + 1) * math.log2(2 / (t + 1)) + t * math.log2(t))
-
-    f_JS_t_actual = calc_f_JS_t(t)
-    assert math.isclose(f_JS_t_actual, f_JS_t_expected)
-
-
-def test_calc_rank_aware_JS_divergence() -> None:
-    P_asterisk = {"a": 0.2, "b": 0.3, "c": 0.5}
-    Q_asterisk = {"a": 0.3, "b": 0.3, "c": 0.4}
-
-    rank_aware_JS_div_expected = (
-        0.3 * calc_f_JS_t(0.2 / 0.3)
-        + 0.3 * calc_f_JS_t(0.3 / 0.3)
-        + 0.4 * calc_f_JS_t(0.5 / 0.4)
-    )
-    rank_aware_JS_div_actual = calc_rank_aware_JS_divergence(P_asterisk, Q_asterisk)
-
-    assert math.isclose(rank_aware_JS_div_actual, rank_aware_JS_div_expected)
