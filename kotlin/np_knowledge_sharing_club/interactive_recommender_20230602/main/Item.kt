b3969bb1656f75d734afe9a8620c223d7983f40d
Masato Morita <masato.morita@uzabase.com>
2023-06-02 19:06:05 +0900

    fix

--- kotlin/np_knowledge_sharing_club/interactive_recommender_20230602/main/Item.kt
+++ kotlin/np_knowledge_sharing_club/interactive_recommender_20230602/main/Item.kt
@@ -1,7 +1,6 @@
 data class Item(
     val id: String,
     val title: String,
-    val score: Double,
     // 記事カテゴリ(business, finance, career, eduacition, technology, industry の6種)
     val category: String,
     val complexity: Double,
