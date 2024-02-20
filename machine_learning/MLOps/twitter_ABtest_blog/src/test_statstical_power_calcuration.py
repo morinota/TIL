b7d31b779b56070706327611bc5f7b8e3a060292
morinota <morinota416@gmail.com>
2024-02-20 22:33:55 +0900

    add

--- machine_learning/MLOps/twitter_ABtest_blog/src/test_statstical_power_calcuration.py
+++ machine_learning/MLOps/twitter_ABtest_blog/src/test_statstical_power_calcuration.py
@@ -109,11 +109,11 @@ def test_calculate_statistical_power() -> None:
     null_dist_mean = 0.5
     null_dist_std = np.sqrt(0.5 * (1 - 0.5) / n)
     null_distribution = NormalDistribution(mean=null_dist_mean, std=null_dist_std)
-
+    print(f"null_dist: mean={null_dist_mean}, std={null_dist_std}")
     alternative_dist_mean = 0.64
     alternative_dist_std = np.sqrt(0.64 * (1 - 0.64) / n)
     alternative_distribution = NormalDistribution(mean=alternative_dist_mean, std=alternative_dist_std)
-
+    print(f"alternative_dist: mean={alternative_dist_mean}, std={alternative_dist_std}")
     acceptable_false_positive_rate = 0.05
     alternative_type = "larger"
 
@@ -126,5 +126,5 @@ def test_calculate_statistical_power() -> None:
     )
 
     # Assert
-    expected_statistical_power = 0.8
-    assert actual_statistical_power == expected_statistical_power
+    expected_statistical_power = 0.886
+    assert np.isclose(actual_statistical_power, expected_statistical_power, atol=0.01)
