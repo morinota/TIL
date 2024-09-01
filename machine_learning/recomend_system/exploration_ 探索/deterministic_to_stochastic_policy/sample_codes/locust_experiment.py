b63c7f92c753f5460368ba0c086223b782188453
morita <morinota416@gmail.com>
2024-09-01 16:21:43 +0900

    add

--- machine_learning/recomend_system/exploration_ 探索/deterministic_to_stochastic_policy/sample_codes/locust_experiment.py
+++ machine_learning/recomend_system/exploration_ 探索/deterministic_to_stochastic_policy/sample_codes/locust_experiment.py
@@ -1,9 +1,66 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, between, task
+
+# サンプルコード
+# class MyUser(HttpUser):
+#     wait_time = between(1, 5)
+
+#     @task(weight=1)
+#     def test_httpbin_get(self):
+#         self.client.get("/get")
+
+#     @task(weight=1)
+#     def test_httpbin_post(self):
+#         self.client.post("/post", json={"key": "value"})
 
 
-class MyUser(HttpUser):
+class recommendedMovieUser(HttpUser):
     wait_time = between(1, 5)
+    USER_ID = "U:114521"
+    K = 20
+    ENDPOINT = f"/temp/recommended-movies/{USER_ID}"
+    REQUEST_HEADER = {"content-type": "application/json", "accept": "application/json"}
 
-    @task
-    def test_httpbin(self):
-        self.client.get("/get")
+    @task(weight=1)
+    def test_deterministic(self) -> None:
+        inference_type = "deterministic"
+        self.client.get(
+            url=self.ENDPOINT,
+            params={"inference_type": inference_type, "k": self.K},
+            headers=self.REQUEST_HEADER,
+        )
+
+    @task(weight=1)
+    def test_stochastic_plackett_luce(self) -> None:
+        inference_type = "stochastic_plackett_luce"
+        self.client.get(
+            url=self.ENDPOINT,
+            params={"inference_type": inference_type, "k": self.K},
+            headers=self.REQUEST_HEADER,
+        )
+
+    @task(weight=1)
+    def test_stochastic_plackett_luce_cached(self) -> None:
+        inference_type = "stochastic_plackett_luce_cached"
+        self.client.get(
+            url=self.ENDPOINT,
+            params={"inference_type": inference_type, "k": self.K},
+            headers=self.REQUEST_HEADER,
+        )
+
+    @task(weight=1)
+    def test_stochastic_gumbel_softmax_trick(self) -> None:
+        inference_type = "stochastic_gumbel_softmax_trick"
+        self.client.get(
+            url=self.ENDPOINT,
+            params={"inference_type": inference_type, "k": self.K},
+            headers=self.REQUEST_HEADER,
+        )
+
+    @task(weight=1)
+    def test_stochastic_epsilon_greedy(self) -> None:
+        inference_type = "stochastic_epsilon_greedy"
+        self.client.get(
+            url=self.ENDPOINT,
+            params={"inference_type": inference_type, "k": self.K},
+            headers=self.REQUEST_HEADER,
+        )
